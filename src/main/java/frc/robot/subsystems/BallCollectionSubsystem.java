381f99ea54a3996bc0aeefbdc592fb87f1de2e47
Paul Foster <restofpaul@gmail.com>
2021-03-06 18:00:18 +0000

    Added empty BallCollectionSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import frc.robot.Map;
+
+public class BallCollectionSubsystem {
+
+    public BallCollectionSubsystem() {
+    }
+
+}
