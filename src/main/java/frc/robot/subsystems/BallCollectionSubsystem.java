d33ac70fffbbcf79d82b94d22729d15bb3906ca9
Nathaniel Foster <restofnate@gmail.com>
2021-03-07 01:53:20 +0000

    chimkin

--- src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
@@ -4,17 +4,20 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import frc.robot.Map;
-
+import java.lang.Math;
 public class BallCollectionSubsystem {
 
     public BallCollectionSubsystem() {
  
     }
-       public void setchimkin() {
-            
-            
-            
-            // set 1-10 
+    
+       public double setchimkin(double a, double b) {
+             
+           double asquared= Math.pow(a,2);
+            double bsquared=Math.pow(b,2);
+           return   Math.pow(asquared + bsquared,.5) ;
+          
+           // set 1-10 
             //if then 1-4 rais arm full speed
             //if then 5-10 raise arm .5
         }
