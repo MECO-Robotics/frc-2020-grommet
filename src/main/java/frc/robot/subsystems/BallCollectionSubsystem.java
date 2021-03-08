34f280c1e13280abd48b2e718dfbcb10b6b54a43
Paul Foster <restofpaul@gmail.com>
2021-03-08 09:47:20 +0000

    Added comments

--- src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
@@ -23,8 +23,12 @@ public class BallCollectionSubsystem {
      * be pressed at a time, but the condition is possible, so it needs to be
      * handled. If neither button is currently pressed, it may mean the arm
      * needs to keep moving or it could mean it's time to stop it.
-     * @param upButton Move the arm all the way up
-     * @param downButton Move the arm all the way down
+     *
+     * @param upButton         Move the arm all the way up
+     * @param downButton       Move the arm all the way down
+     * @param topLimintSwich   Normally closed limit switch. When arm reaches top, this goes false
+     * @param bottomLimitSwich Normally closed bottom limit switch. When arm reaches bottom, this goes false
+     *
      * @return The motor speed to apply to the lifter arm
      */
     public double computeArmMotorSpeed(boolean upButton, boolean downButton, boolean topLimintSwich, boolean bottomLimitSwich) {
@@ -48,26 +52,16 @@ public class BallCollectionSubsystem {
     
     }
     
+    
+    
     /**
      * Determine the roller motor speed given the intakeTrigger level and
      * the outTakeTrigger level. If both triggers are pulled, or neither are pulled,
      * the motor speed should be 0.
      */
-    public double computeRollerSpeed(double intakeTrigger, double outTakeTrigger) {
+    public double computeRollerSpeed(double intakeTriggerLevel, double outTakeTriggerLevel) {
         return 0.0;
     }
-    
-       public double setchimkin(double a, double b) {
-             
-           double asquared= Math.pow(a,2);
-            double bsquared=Math.pow(b,2);
-           return   Math.pow(asquared + bsquared,.5) ;
-          
-           // set 1-10 
-            //if then 1-4 rais arm full speed
-            //if then 5-10 raise arm .5
-        }
-    
     
 }
 
