64e94dbf865e8950d6afe6a36291703df54ad726
Paul Foster <79760895+paul-foster@users.noreply.github.com>
2021-03-10 21:15:07 -0500

    Added method prototypes to AutonomousRoutine.java

---
+++ src/main/java/frc/robot/AutonomousRoutine.java
@@ -0,0 +1,48 @@
+package frc.robot;
+
+/**
+ * Stores an autonomous program the robot can execute.
+ */
+class AutonomousRoutine {
+
+    public AutonomousRoutine() {
+    }
+    
+    /**************** Teleop Record Methods **************/
+    
+    public void startRecording(String programName) {
+    }
+    
+    public void stopRecording() {
+    }
+    
+    /**
+     * Record the state of the bot for the current time period
+     */
+    public void recordState() {
+    }
+
+    /*****************  Autonomous Methods ****************/
+    
+    
+    public void startAutonomous(String programName) {
+    }
+    
+    public void cancelAutonomous() {
+    }
+    
+    public boolean isComplete() {
+    }
+    
+    public double getLeftMotorSpeed() {
+    }
+    
+    public double getRightMotorSpeed() {
+    }
+    
+    public double getBallCollectionArmLiftSpeed() {
+    }
+    
+    public double getBallCollectionIntakeRotorSpeed() {
+    }
+}
