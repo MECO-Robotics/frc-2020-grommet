c4cbc3265711b4d9f734770ced5a4af8d5b1c8a0
Paul Foster <restofpaul@gmail.com>
2021-03-31 16:11:44 -0400

    Added IBallCollectionSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/BallCollectionSubsystem.java
@@ -0,0 +1,23 @@
+package frc.robot.subsystems;
+
+public class BallCollectionSubsystem implements IBallCollectionSubsystem {
+
+    @Override
+    public void raiseArm() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void lowerArm() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void engageBallCollectionRoller(double level) {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
---
+++ src/main/java/frc/robot/subsystems/IBallCollectionSubsystem.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+public interface IBallCollectionSubsystem {
+
+    void raiseArm();
+    void lowerArm();
+    void engageBallCollectionRoller(double level);
+}
